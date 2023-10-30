3b3e43fc4e340052f12055137231d88f4333be38
Claudio Dall'Ara <claudiodallara77@gmail.com>
2023-10-30 22:05:22 +0100

    little little mod

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,7 +3,7 @@ const factory = require('./handlerFactory');
 
 // new version with factory file
 exports.getAllProducts = factory.getAll(Product);
-exports.getProduct = factory.getOne(Product); //--------------da capire!!!!!!!
+exports.getProduct = factory.getOne(Product);
 exports.createProduct = factory.createOne(Product);
 // ps DONT update passwords with this
 exports.updateProduct = factory.updateOne(Product);
