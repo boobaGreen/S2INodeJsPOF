b1810b6f3ab4b4410f3535d6240844cc45ee8b7a
Claudio Dall'Ara <claudiodallara77@gmail.com>
2023-10-30 13:11:38 +0100

    first deploy test

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const compression = require('compression');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const userRouter = require('./routes/userRoutes');
@@ -43,6 +44,8 @@ app.use(
     whitelist: ['createdAt', 'name', 'email', 'surname'],
   }),
 );
+
+app.use(compression());
 
 // ROUTES
 app.use('/api/v1/products', productRouter);
--- models/orderModel.js
+++ models/orderModel.js
@@ -41,16 +41,6 @@ orderSchema.pre(/^find/, function (next) {
   next();
 });
 
-// reviewSchema.pre(/^findOneAnd/, async function (next) {
-//   this.r = await this.model.findOne(this.getQuery());
-//   //console.log(this.r);
-//   next();
-// });
-
-// reviewSchema.post(/^findOneAnd/, async function () {
-//   // await this.findOne(); does NOT work here, query has already executed
-//   await this.r.constructor.calcAverageRatings(this.r.tour);
-// });
 const Orders = mongoose.model('Order', orderSchema);
 
 module.exports = Orders;
