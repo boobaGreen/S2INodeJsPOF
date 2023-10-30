bc35b6671a117c6113097e1eff9f8bafdf2b7801
Claudio Dall'Ara <claudiodallara77@gmail.com>
2023-10-30 22:02:21 +0100

    little little mod

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,3 +1,5 @@
+// this avoids repeating the catct and try blocks
+// only for aesthetics and better readability of the main code
 module.exports = (fn) => (req, res, next) => {
   fn(req, res, next).catch((err) => next(err));
 };
